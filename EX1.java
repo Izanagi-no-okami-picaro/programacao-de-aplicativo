533e6c6f1e7aa6870f1acb4178fa67f2dbfc994d
Artie Batista & João Victor Duarte <107406612+Izanagi-no-okami-picaro@users.noreply.github.com>
2022-06-27 12:04:50 -0300

    Add files via upload

---
+++ EX1.java
@@ -0,0 +1,29 @@
+package pagina_26;
+
+import java.util.Scanner;
+
+public class exer1{
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+           
+            float h, b, B, area;
+
+          
+            System.out.print("Digite a altura do trapézio(h): ");
+            h = input.nextFloat();
+
+            System.out.print("Digite a base menor do trapézio(b): ");
+            b = input.nextFloat();
+
+            System.out.print("Digite a base maior do trapézio(B): ");
+            B = input.nextFloat();
+
+           
+            area = (float)(h * (b +B)) / 2;
+
+            
+            System.out.print("A área do trapézio é " + area);
+        }
+
+    }
+}
---
+++ EX3.java
@@ -0,0 +1,15 @@
+package pagina_26;
+
+public class exer3 {
+    public static void main(String[] args) {
+
+       
+        System.out.println("a = " + (3 - 2 - 1 + 2 + 1 + 3));
+
+        System.out.println("b = " + ((2 * 3) - (4 * 5)));
+
+        System.out.println("c = " + (2 + 6 - ((3 / 7) * 9)));
+
+        System.out.println("d = " + ((3 % 4) - 8));
+    }
+}
